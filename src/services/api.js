89a7347de285069a82022d45514f456a78e76535
AliKhMahmoud <Ali.Mahmoud.135246@gmail.com>
2026-08-04 18:37:05 +0300

    ...

--- src/services/api.js
+++ src/services/api.js
@@ -2,20 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
-  withCredentials: true, 
+  withCredentials: true, // هذه هي الأساس والأهم لتنتقل الـ Cookies تلقائياً بين Vercel و Render
 });
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("sportshub_token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default api;
